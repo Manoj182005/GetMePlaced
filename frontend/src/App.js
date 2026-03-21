4d39161da70268f3140385715196f13841523140
Manoj Patel BM <manojpatelbm182005@gmail.com>
2026-03-21 23:42:50 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,16 @@
 import './App.css';
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import ResumeUpload from "./components/ResumeUpload";
 import InterviewChat from "./components/InterviewChat";
 
 function App() {
-
-  const [activeTab, setActiveTab] = useState("resume");
-
   return (
     <div className="App">
       <Navbar />
 
-      {/* Center Box */}
-      <div className="main-container">
-
-        {/* Toggle Icons */}
-        <div className="toggle-box">
-          <button
-            className={activeTab === "resume" ? "toggle-btn active" : "toggle-btn"}
-            onClick={() => setActiveTab("resume")}
-          >
-            📄
-          </button>
-
-          <button
-            className={activeTab === "chat" ? "toggle-btn active" : "toggle-btn"}
-            onClick={() => setActiveTab("chat")}
-          >
-            💬
-          </button>
-        </div>
-
-        {/* Content Box */}
-        <div className="content-card">
-          {activeTab === "resume" && <ResumeUpload />}
-          {activeTab === "chat" && <InterviewChat />}
-        </div>
-
+      <div className="main-layout">
+        <ResumeUpload />
+        <InterviewChat />
       </div>
     </div>
   );
