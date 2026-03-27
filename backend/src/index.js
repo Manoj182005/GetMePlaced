c329e748a81f0036cc9762ccf7d9092ac5f79b16
Manoj Patel BM <manojpatelbm182005@gmail.com>
2026-03-27 10:26:53 +0530

    Mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import { initializeRAG } from "./services/rag.service.js";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
-
+console.log("Mongo URI:", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => {
+    console.log("❌ MongoDB Error:", err.message);
+  });
 app.listen(PORT, async() => {
   console.log(`Server running on port ${PORT}`);
 
